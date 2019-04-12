39ae5a64206e59c1b90eea25d86e93067e528826
lenovo <501946152>
2019-04-12 17:29:34 +0800

    bar

---
+++ docs/precache-manifest.1044fb746c22dcc3907a9eb4493f05ab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "9887ed4185074cb8814a",
+    "url": "js/chunk-vendors.25cf0461.js"
+  },
+  {
+    "revision": "902c0f661294648ce389",
+    "url": "js/app.1589cae6.js"
+  },
+  {
+    "revision": "4569afadab7b1fa9265f",
+    "url": "js/about.26236d9b.js"
+  },
+  {
+    "revision": "3fb203fe268184fe1c2c9446d4e035c8",
+    "url": "index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "902c0f661294648ce389",
+    "url": "css/app.28551229.css"
+  },
+  {
+    "revision": "4569afadab7b1fa9265f",
+    "url": "css/about.776bd544.css"
+  }
+];
